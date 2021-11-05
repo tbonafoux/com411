aa6b87921d238367c7e89b070858e5f133f323f4
unknown <5bonat64@solent.ac.uk>
2021-11-05 12:51:34 +0000

    titanic task code upload

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -19,7 +19,8 @@ def display_menu():
           "[1] Display the names of all passengers\n"
           "[2] Display the number of passengers that survived\n"
           "[3] Display the number of passengers per gender\n"
-          "[4] Display the number of passengers per age group\n")
+          "[4] Display the number of passengers per age group\n"
+          "[5] Check if the main 2 Titanic movie characters where based on real people\n")
     user_selection = int(input())
     print(f"You have selected option {user_selection}")
     return user_selection
@@ -41,6 +42,50 @@ def display_number_survived():
     print(f"{num_survived} passengers survived")
 
 
+def display_passengers_per_gender():
+    global records
+    females = 0
+    males = 0
+    for passengers in records:
+        if passengers[4] == "male":
+            males += 1
+        elif passengers[4] == "female":
+            females += 1
+    print(f"Females: {females}, Males: {males}")
+
+
+def display_passengers_per_age_group():
+    global records
+    children = 0
+    adults = 0
+    elderly = 0
+    for passengers in records:
+        if (passengers[5]) == "":
+            pass
+        elif float(passengers[5]) < 18:
+            children += 1
+        elif float(passengers[5]) < 65:
+            adults += 1
+        else:
+            elderly += 1
+    print(f"children: {children}, adults: {adults}, elderly: {elderly}")
+
+
+def movie_test():
+    global records
+    rose = False
+    jack = False
+    for passenger in records:
+        if "Rose" in passenger[3] and int(passenger[1] == 1):
+            rose = True
+    for passenger in records:
+        if "Jack" in passenger[3] and int(passenger[1] == 0):
+            jack = True
+    if rose == True and jack == True:
+        print(f"Possibly based on real people")
+    else:
+        print(f"Not based on real people")
+
 def run():
     load_data("titanic.csv")
     print(f"Successfully loaded {len(records)} records.")
@@ -50,9 +95,11 @@ def run():
     elif user_selection == 2:
         display_number_survived()
     elif user_selection == 3:
-        print("no")
+        display_passengers_per_gender()
     elif user_selection == 4:
-        print("no")
+        display_passengers_per_age_group()
+    elif user_selection == 5:
+        movie_test()
     else:
         print("Error! Option not recognised!")
 
