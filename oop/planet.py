e30d77ef31efdbf287e28944bf49334d17a3448b
unknown <5bonat64@solent.ac.uk>
2021-12-10 11:39:14 +0000

    planet class code

--- oop/planet.py
+++ oop/planet.py
@@ -1,30 +1,47 @@
-from oop.human import Human
-from oop.robot import Robot
+from human import Human
+from robot import Robot
 
 
 class planet:
 
-    humans = []
-    robots = []
-
-    def add_human(self, a, b, c):
-        a = Human(a, b, c)
-        self.humans.append(a)
-
-    def remove_human(self):
-        self.humans.remove(Human())
-
-    def add_robot(self):
-        self.robots.append(Robot())
-
-    def remove_robot(self):
-        self.robots.remove(Robot())
+    def __init__(self, name = ""):
+        self.name = name
+        self.occupants = {
+            "humans": [],
+            "robots": []
+        }
 
     def __repr__(self):
-        return f"Humans; {humans}\n" \
-               f"Robots; {Robots}"
+        return f"[{self.name}:\n" \
+               f"Humans; {self.occupants['humans']}\n" \
+               f"Robots; {self.occupants['robots']}"
+
+    def __str__(self):
+        return f"{self.name} has the {len(self.occupants['humans'])} humans and {len(self.occupants['robots'])} robots"
+
+    def add_human(self, human):
+        self.occupants["humans"].append(human)
+
+    def remove_human(self, human):
+        self.occupants["humans"].remove(human)
+
+    def add_robot(self, robot):
+        self.occupants["robots"].append(robot)
+
+    def remove_robot(self, robot):
+        self.occupants["robots"].remove(robot)
 
 
-planet.add_human("bob", 20, 50)
-planet.add_human("rob", 30, 66)
-print(repr(planet))
+if __name__ == "__main__":
+    dune = planet("Dune")
+    print(repr(dune))
+    robby = Human("Rob")
+    dune.add_human(robby)
+    bob = Robot("Bob", 20, 50)
+    dune.add_robot(bob)
+    beep = Robot("Beep", 100, 20)
+    dune.add_robot(beep)
+    print(dune)
+    dune.remove_robot(beep)
+    print(repr(dune))
+    print(dune)
